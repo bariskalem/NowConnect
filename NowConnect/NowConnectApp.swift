c9c307cfa7f88fde11f9322360d46afb03acfbc8
Baris Kalem <baris.kalem03@bilgiedu.net>
2023-09-01 16:16:16 +0200

    Initial Commit

---
+++ NowConnect/NowConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowConnectApp.swift
+//  NowConnect
+//
+//  Created by Barış Kalem on 01.09.23.
+//
+
+import SwiftUI
+
+@main
+struct NowConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
